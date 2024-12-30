1dcb71d75c533259860fc4f0f170f778eb2eef97
DESKTOP-ON220TB\Notebook <matteo.candiani08@gmail.com>
2024-12-30 14:21:41 +0100

    CANDIANI-aggiunta movimento e rimozione del lampeggiare dovuto al clearscreen

--- asrtonavi/Origine.cpp
+++ asrtonavi/Origine.cpp
@@ -1,20 +1,48 @@
+//dimesioni dell'esecutore
 #define IMM2D_WIDTH 300
 #define IMM2D_HEIGHT 150
+
 #define IMM2D_IMPLEMENTATION
 #include <iostream>
 #include "immediate2d.h"
 using namespace std;
 
+//immagine dell'astronave
 static constexpr const char astronavepng[] = "immagineastronave.PNG";
 
- 
 void run() {
-	int x = 145;
-	int y = 100;
+	//coordinate iniziali dell'astronave (inseguito si modificano)
+    int x = 145;
+    int y = 100;
 
-	
-	const Image astronave = LoadImage(astronavepng);
+	//carica l'immagine dell'astronave
+    const Image astronave = LoadImage(astronavepng);
+    
+    //per un codice dove l'atronave non lampeggia 
+    UseDoubleBuffering(true);
+	//il ciclo continua finche la variabile esecuzione è true
+    bool esecuzione = true;
 
-	DrawImage(x, y, astronave);
 
+
+    while (esecuzione) {
+		//l'ultimo tasto premuto permette il movimento dell'astronave
+        const char key = LastKey();
+        if (key == Left) x -= 5;
+        if (key == Right) x += 5;
+        if (key == Up) y -= 5;
+        if (key == Down) y += 5;
+
+        // cancella la precedente astronave
+        Clear(Black);
+        
+		// Disegna l'immagine alle coordinate alle nuove coordinate
+        DrawImage(x, y, astronave);
+
+        
+        Present();
+
+        
+    }
 }
+
